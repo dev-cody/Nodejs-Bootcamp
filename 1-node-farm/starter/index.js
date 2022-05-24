5fde34559731f649a88d56462c249a8549b8e79b
Cody Koscielski <codykoscielski@gmail.com>
2022-05-24 16:29:11 -0400

    Created our first API
    Created our first API and retuned a simple JS Object

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+
 //******************************** */
 //File Readt
 //Blocking, synchornous way
@@ -21,6 +22,10 @@ const url = require('url')
 
 
 //Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
     const pathName = req.url
 
@@ -28,6 +33,9 @@ const server = http.createServer((req, res) => {
         res.end('this is the overview')
     } else if(pathName === '/product') {
         res.end('this is the product')
+    } else if( pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
     } else {
         res.writeHead(404, {
             'content-type': 'text/html',
